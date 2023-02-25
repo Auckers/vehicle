23ee04ef399a577c1531fcf70991a93e347f0b4a
Krishan Agarwal <2017UEE1640@mnit.ac.in>
2023-02-25 21:34:12 +0530

    GetAndPostImage

--- src/main/java/com/vehicle/controller/VehicleController.java
+++ src/main/java/com/vehicle/controller/VehicleController.java
@@ -3,8 +3,18 @@ package com.vehicle.controller;
 import com.vehicle.model.domain.Vehicle;
 import com.vehicle.service.VehicleRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -12,6 +22,8 @@ public class VehicleController {
 
     @Autowired
     VehicleRegistrationService vehicleRegistrationService;
+
+    String basePath="C:\\Users\\krish\\Desktop\\Rentler\\";
 
     @RequestMapping(value = "/addVehicle", method = RequestMethod.POST)
     public String addVehicle(@RequestBody Vehicle vehicle){
@@ -43,4 +55,35 @@ public class VehicleController {
             return null;
         }
     }
+    
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        String filename = file.getOriginalFilename();
+        Path filepath = Paths.get(basePath + filename);
+        try {
+            Files.createDirectories(filepath.getParent());
+            file.transferTo(filepath);
+            return "File uploaded successfully "+filename;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to upload file";
+        }
+    }
+
+    @RequestMapping(value = "/getImage", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getImage(@RequestParam("imageName") String imageName) throws IOException {
+        System.out.println("Got image name to retrive "+imageName);
+        String pathname=basePath+imageName;
+        System.out.println(pathname+" created");
+        File file = new File(pathname);
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
+    }
+    /*TODO
+     * Currently front end not able to handle the byte[] received
+     * For now Get Mapping is used but need to change this to POST Mapping
+     * And also handle the byte[] to display image
+     * */
 }
